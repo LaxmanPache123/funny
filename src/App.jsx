0934ae98cdf8f64879cb1f1bff6d314df37d8337
LaxmanPache123 <laxman.p@solytics-partners.com>
2022-11-19 21:03:25 +0530

    [005] when app js file changed then compalsary to changed app css file in precommit hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <div>Testinffgsss {process.env.REACT_APP_SECREAT_KEY}</div>
+  return <div>Testisdsfdsnffgsss {process.env.REACT_APP_SECREAT_KEY}</div>
 }
 
 export default App

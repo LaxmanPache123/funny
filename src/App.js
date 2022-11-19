07db2dea708041de76740bcc12418fdedc83fbe3
LaxmanPache123 <laxman.p@solytics-partners.com>
2022-11-19 19:00:23 +0530

    eslint preteer husky setup

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>Testing {process.env.REACT_APP_SECREAT_KEY}</div>
-  )
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function App() {
+  return <div>Testinffg {process.env.REACT_APP_SECREAT_KEY}</div>
+}
+
+export default App

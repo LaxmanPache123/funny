b058ce05888cc69d25d14563ee5246fc5fa495ab
LaxmanPache123 <laxman.p@solytics-partners.com>
2023-01-24 00:39:23 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function App() {
-  return <div>Testisdsfdsnffgsss {process.env.REACT_APP_SECREAT_KEY}dsdssdfs</div>
+  return (
+    <div>Testisdsfddsfsdsnffgsss {process.env.REACT_APP_SECREAT_KEY}dsdssdfs</div>
+  )
 }
 
 export default App

2a1c18aa538077bd1841200cfc9fa02dff7797d9
Laxman <97151206+LaxmanPache123@users.noreply.github.com>
2024-01-29 13:43:46 +0530

    Update App.jsx
    Update App.jsx for template test msg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Testisdsfddsfsdsnffgsss {process.env.REACT_APP_SECREAT_KEY}dsdssdfs</div>
+    <div> template Test {process.env.REACT_APP_SECREAT_KEY}dsdssdfs</div>
   )
 }
 

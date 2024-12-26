277f18d192d9dce50fe2fa69c9aaea784027e491
Laxman Pache <laxman.p@solytics-partners.com>
2024-12-26 12:13:34 +0530

    testing 2nd machine for runner

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Testisdsfddsfsdsnffgsss  testing runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs</div>
+    <div>Testisdsfddsfsdsnffgsss  testing runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
   )
 }
 

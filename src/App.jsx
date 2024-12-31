988511fd18dab9a955a666ce456154b7e791f58d
Laxman Pache <laxman.p@solytics-partners.com>
2024-12-31 16:47:33 +0530

    sdfsfs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Testisdsfddsfsdsnffgsss  sdsd sdftesting runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
+    <div>Testisdsfddsfsdsnfsddfgsss  sdsd sdftesting runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
   )
 }
 

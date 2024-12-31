5845956fa29814d466ba73567489fb18c422a182
Laxman Pache <laxman.p@solytics-partners.com>
2024-12-31 16:57:21 +0530

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Testisdsfddsfsdsnfsddssdfsfsdfsfgsss  sdsd sdftesting runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
+    <div>
+      Testisdsfddsfsdsnfsddssdfsfsssdddfsfgsss sdsd sdftesting runner{' '}
+      {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing
+    </div>
   )
 }
 

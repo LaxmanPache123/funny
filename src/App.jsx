d1bc191b388dd0868dc1f9755526412cb3d33262
Laxman Pache <laxman.p@solytics-partners.com>
2024-12-31 16:50:59 +0530

    lint staged

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Testisdsfddsfsdsnfsddsdfsfgsss  sdsd sdftesting runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
+    <div>Testisdsfddsfsdsnfsddssdfsfsdfsfgsss  sdsd sdftesting runner {process.env.REACT_APP_SECREAT_KEY}dsdssdfs testing</div>
   )
 }
 
